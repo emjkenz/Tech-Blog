026e6baa1d17d8f9ee44bf55f23d3024bb16af19
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-06-13 21:43:18 +1000

    Import controller routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
-const { homeController } = require('../controllers');
+const { 
+    homeController,
+    dashboardController,
+    loginController,
+    logoutController,
+    postController,
+} = require('../controllers');
 
+router.use('/dashboard', dashboardController);
+router.use('/post', postController);
+router.use('/login', loginController);
+router.use('/logout', logoutController);
 router.use('/', homeController);
 
 module.exports = router;
