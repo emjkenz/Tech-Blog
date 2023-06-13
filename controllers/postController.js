15752c20eaf1ce680c06c8b4a9309cee9f41f782
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-06-13 21:42:38 +1000

    Added post controller

---
+++ controllers/postController.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.post('/', withAuth, (req, res) => {
+    const {title, post, id} = req.body;
+    Post.create({
+        title: title,
+        post: post,
+        date: new Date(),
+        author_id: id,
+    }).then( err => {
+        res.redirect('/dashboard');
+    })
+});
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        },
+        include: {
+            model: User,
+        }
+        
+    }).then(post => {
+        const p = post.get({ plain: true })
+        res.render('post', {...p, ...{isLoggedIn: req.session.isLoggedIn}});
+    })
+})
+
+module.exports = router;
