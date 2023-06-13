24c570c46fa6ea9bf7cbdcadc6b7365ce819cf6b
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-06-13 21:43:39 +1000

    Added password validator function

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model { }
+class User extends Model { 
+    isValidPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+}
 
 User.init(
     {
