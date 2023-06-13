a2c6bc2cc8f1c14f3426d0f3de3d24fc3de0d043
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-06-13 21:42:59 +1000

    Added controller exports

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,13 @@
 const homeController = require('./homeController');
+const dashboardController = require('./dashboardController');
+const loginController = require('./loginController');
+const logoutController = require('./logoutController');
+const postController = require('./postController');
 
 module.exports = {
-    homeController
+    homeController,
+    dashboardController,
+    loginController,
+    logoutController,
+    postController,
 };
