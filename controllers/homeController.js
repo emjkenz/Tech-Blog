4905be03e686e6a910d77feb81cc706ee55b7511
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-06-13 21:39:45 +1000

    Updated home controller to get posts

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,17 @@
+const { Post, User } = require("../models"); 
+
 const home = (req, res) => {
-    res.render('home');
+    Post.findAll({
+        order: [['date', 'DESC']],
+        include: {
+            model: User,
+        }
+    })
+    .then(posts => {
+        const allPosts = posts.map(post => post.get({plain:true}))
+        res.render('home', {posts: allPosts, isLoggedIn: req.session.isLoggedIn});
+    });
+    
 }
 
 module.exports = home;
