827e297535ee0eb776b8a69c79541f894fc3fb28
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-06-14 00:27:44 +1000

    Added comments

--- controllers/postController.js
+++ controllers/postController.js
@@ -25,28 +25,42 @@ router.get('/:id', async (req, res) => {
             }
         })
 
-        // const comments = await Comment.findAll({
-        //     where: {
-        //         post_id: req.params.id,
-        //     }
-        // });
+        const comments = await Comment.findAll({
+            where: {
+                post_id: req.params.id,
+            },
+            include: {
+                model: User,
+            }
+        });
+
+        const allComments = comments.map(comment => comment.get({plain:true}))
+
         const p = post.get({ plain: true })
-        res.render('post', { ...p, ...{ pageTitle: " | " + p.title, isLoggedIn: req.session.isLoggedIn } });
+        res.render('post', {
+            ...p,
+            ...{
+                pageTitle: " | " + p.title,
+                isLoggedIn: req.session.isLoggedIn,
+                postID: req.params.id,
+                comments: allComments,
+            }
+        });
     } catch (error){
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
-    }
-    
+    } 
 })
 
-router.post('/:id/comment', async (req, res) => {
-    const { postId } = req.params;
-    const { id, content } = req.body;
+router.post('/:id/comment', withAuth, async (req, res) => {
+    const postId = req.params.id;
 
+    const { user, content } = req.body;
     try {
         // Find the post by ID
         const post = await Post.findByPk(postId);
         if (!post) {
+            console.log("error");
             return res.status(404).json({ error: 'Post not found' });
         }
 
@@ -54,9 +68,8 @@ router.post('/:id/comment', async (req, res) => {
         const newComment = await Comment.create({
             content,
             post_id: postId, // Associate the comment with the post using the foreign key
-            user_id: id,
+            user_id: user,
         });
-
         // Respond with the created comment
         res.redirect(`/post/${postId}`);
     } catch (error) {
